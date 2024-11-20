f89078a2d61dc23af25e7404c5f38003abc038de
Julia123456789037 <130442095+Julia123456789037@users.noreply.github.com>
2024-11-20 14:53:34 +0100

    Create Q6.java

---
+++ Q6.java
@@ -0,0 +1,20 @@
+/** 
+ * 
+ * @author BONDU justine
+ * @version 1 du 
+ */
+
+public class Q6
+{
+
+	public static void main(String[] argv)
+	{
+		/*-----------------*/
+		/* Instructions    */
+		/*-----------------*/
+
+		
+		System.out.println ( " ********** ");
+		
+	}
+}
