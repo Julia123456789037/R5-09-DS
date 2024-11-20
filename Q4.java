fb5c0d30cf8b8e62bd2ea3f98e5e5a13dc1349ec
Julia123456789037 <130442095+Julia123456789037@users.noreply.github.com>
2024-11-20 14:25:03 +0100

    Create Q4.java

---
+++ Q4.java
@@ -0,0 +1,22 @@
+/** 
+ * 
+ * @author BONDU justine
+ * @version 1 du 
+ */
+
+public class LigneEtoile
+{
+
+	public static void main(String[] argv)
+	{
+		/*-----------------*/
+		/* Instructions    */
+		/*-----------------*/
+
+		System.out.println ( "*");
+	    System.out.println ( "**");
+	    System.out.println ( "***");
+	    System.out.println ( "****");
+		
+	}
+}
