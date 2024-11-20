05a327085e278e8b3b2933710bc8ca358ae157f6
Julia123456789037 <130442095+Julia123456789037@users.noreply.github.com>
2024-11-20 13:59:45 +0100

    Create Q3.java

---
+++ Q3.java
@@ -0,0 +1,18 @@
+/** 
+ * 
+ * @author BONDU justine
+ * @version 1 du 
+ */
+
+public class LigneEtoile
+{
+
+	public static void main(String[] argv)
+	{
+		/*-----------------*/
+		/* Instructions    */
+		/*-----------------*/
+
+    for (cpt = 0;cpt < argv[0];cpt++) { System.out.print ( "*"); }
+	}
+}
